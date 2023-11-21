ae18b8145bec230cf13d21bb64ecc14191762d1c
nulfrost <khadane.miller@gmail.com>
2023-11-21 15:56:44 -0500

    fixing minor logic with increase and decrease levels

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { create } from "zustand";
 interface TreeState {
   tree: typeof treeJson;
   increaseSkillPoint: (jobId: number, skillId: number) => void;
+  decreaseSkillPoint: (jobId: number, skillId: number) => void;
 }
 
 const useTreeStore = create<TreeState>()((set) => ({
@@ -15,6 +16,7 @@ const useTreeStore = create<TreeState>()((set) => ({
       const job = state.tree.find((j) => j.id === jobId);
       // find skill
       const skill = job?.skills.find((s) => s.id === skillId);
+      if (skill!.skillLevel === skill!.levels.length) return state;
       skill!.skillLevel += 1;
       // find all required skills
       // if it's the min level, switch hasMinLevel to true
@@ -36,6 +38,34 @@ const useTreeStore = create<TreeState>()((set) => ({
         tree: [...state.tree],
       };
     }),
+  decreaseSkillPoint: (jobId: number, skillId: number) =>
+    set((state) => {
+      // find the job
+      const job = state.tree.find((j) => j.id === jobId);
+      // find skill
+      const skill = job?.skills.find((s) => s.id === skillId);
+      if (skill?.skillLevel === 0) return state;
+      skill!.skillLevel -= 1;
+      // find all required skills
+      // if the skillLevel is less than the required skills required level switch to false
+      job?.skills.forEach((s) => {
+        const foundSkill = s.requirements.find((sz) => sz.skill === skillId);
+        const skillIndex = s.requirements.findIndex(
+          (sx) => sx.skill === skillId
+        );
+        if (
+          typeof foundSkill !== "undefined" &&
+          skill!.skillLevel < foundSkill.level
+        ) {
+          s.requirements[skillIndex].hasMinLevel = false;
+        }
+      });
+
+      return {
+        ...state,
+        tree: [...state.tree],
+      };
+    }),
 }));
 
 function App() {
@@ -43,38 +73,42 @@ function App() {
   // we need to check if it meets the min level for the
   // skills it it needed for
   const increaseSkillPoint = useTreeStore((state) => state.increaseSkillPoint);
+  const decreaseSkillPoint = useTreeStore((state) => state.decreaseSkillPoint);
   const tree = useTreeStore((state) => state.tree);
-
-  function logRequirements(skillId: number, skills: any[]) {
-    // on the current skill, look through the current tree skills
-    // find where the skill is a requirement and log the skill name that is dependant
-    console.log(skills);
-    const reqSkills = skills.filter((skill) =>
-      skill.requirements.find((s: any) => s.skill === skillId)
-    );
-
-    return reqSkills;
-  }
 
   return (
     <div className="eleGrid gap-1 p-10">
-      {tree[5].skills
-        // .sort((a, b) => a.level - b.level)
-        .map((skill) => {
-          const hasMinLevelRequirements = skill.requirements.every(
-            (req) => req.hasMinLevel === true
-          );
-          const isMaxed = skill.skillLevel === skill.levels.length;
-          return (
+      {tree[5].skills.map((skill) => {
+        const hasMinLevelRequirements = skill.requirements.every(
+          (req) => req.hasMinLevel === true
+        );
+        const isMaxed = skill.skillLevel === skill.levels.length;
+        return (
+          <>
             <div
               key={skill.id}
               data-skill={skill.name.en.replaceAll(" ", "")}
-              className="flex border border-gray-300 rounded-md"
+              className="flex border border-gray-300 rounded-md flex-col items-center py-2"
             >
               <button
-                onClick={() => increaseSkillPoint(9150, skill.id)}
-                disabled={!hasMinLevelRequirements || isMaxed}
-                className="w-full flex flex-col items-center py-2"
+                onKeyDown={(event) => {
+                  event.preventDefault();
+                  if (event.key === "ArrowDown") {
+                    decreaseSkillPoint(9150, skill.id);
+                  } else if (event.key === "ArrowUp") {
+                    increaseSkillPoint(9150, skill.id);
+                  }
+                }}
+                onClick={(event) => {
+                  console.log(event.type);
+                  if (event.type === "click") {
+                    increaseSkillPoint(9150, skill.id);
+                  } else if (event.type === "contextmenu") {
+                    decreaseSkillPoint(9150, skill.id);
+                  }
+                }}
+                disabled={!hasMinLevelRequirements}
+                className="w-full flex flex-col items-center"
               >
                 <span
                   className={`font-bold block ${
@@ -100,13 +134,22 @@ function App() {
                 </span>
               </button>
               <div>
-                {logRequirements(skill.id, tree[5].skills).map((req) => (
-                  <span>{req.name.en}</span>
+                {skill.requirements.map((skill) => (
+                  <span
+                    className={`block text-sm text-center ${
+                      hasMinLevelRequirements
+                        ? "text-green-500"
+                        : "text-red-500"
+                    }`}
+                  >
+                    {skill.name} level {skill.level} is required
+                  </span>
                 ))}
               </div>
             </div>
-          );
-        })}
+          </>
+        );
+      })}
     </div>
   );
 }
