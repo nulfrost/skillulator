efebf163824ab3376314bc48c092e8c5ad4e876d
nulfrost <khadane.miller@gmail.com>
2023-11-22 20:54:49 -0500

    finish psykeeper tree

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Link, RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  Link,
+  RouterProvider,
+  createBrowserRouter,
+  isRouteErrorResponse,
+  useRouteError,
+} from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
@@ -43,15 +49,15 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: (
-      <div className="flex justify-center items-center h-full flex-col px-3 py-3">
-        <h1 className="text-3xl mb-4 font-bold">Skillulator</h1>
-        <div className="grid grid-cols-4 gap-2 w-full lg:w-max">
+      <div className="flex flex-col items-center justify-center h-full px-3 py-3">
+        <h1 className="mb-4 text-3xl font-bold">Skillulator</h1>
+        <div className="grid w-full grid-cols-4 gap-2 lg:w-max">
           {JOBS.map((job) => (
             <Link
               aria-label={`Go to the ${job.name} skill tree`}
               to={`/c/${job.name}`}
               key={job.name}
-              className="flex items-center border border-gray-300 rounded-md px-1 py-2 hover:bg-gray-100 duration-150 bg-white justify-center lg:px-5 flex-col"
+              className="flex flex-col items-center justify-center px-1 py-2 duration-150 bg-white border border-gray-300 rounded-md hover:bg-gray-100 lg:px-5"
             >
               <img
                 src={`https://api.flyff.com/image/class/target/${job.image}`}
@@ -62,12 +68,23 @@ const router = createBrowserRouter([
         </div>
       </div>
     ),
+    errorElement: <ErrorBoundary />,
   },
   {
     path: "/c/:class",
     element: <App />,
   },
 ]);
+
+function ErrorBoundary() {
+  const error = useRouteError();
+  if (isRouteErrorResponse(error)) {
+    if (error.status === 404) {
+      return <div>Page not found</div>;
+    }
+  }
+  return <div>This is not a normal error, please report this</div>;
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
